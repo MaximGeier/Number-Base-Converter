5b27a7feffcc78f5956bcd4306a53bec97cbdcd1
Max Geyer <maxim.geyer@t-systems.com>
2021-10-17 08:56:28 +0200

    Updated the project

--- Resources.kt
+++
@@ -1,9 +0,0 @@
-package converter
-
-const val EXIT = "/exit"
-const val BASE_PROMPT = "Enter two numbers in format: {source base} {target base} (To quit type $EXIT) "
-
-const val BACK = "/back"
-const val NUMBER_PROMPT = "Enter number in base %s to convert to base %s (To go back type $BACK) "
-
-const val CONVERSION_RESULT = "Conversion result: %s\n"
